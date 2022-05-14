114698869b9dc54a733de98bd3fcc198b114d802
Hossen <mdmitho199911@gmail.com>
2022-05-15 01:44:42 +0600

    Verify JWT Token and handle unauthorized accessv

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
+function verifyJWT (req,res, next){
+  const authHeader = req.headers.authorization
+  if(!authHeader){
+    return res.status(401).send({message:'UnAuthorized Access'})
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if(err){
+      return res.status(403).send({message:'Forbidden access'})
+    }
+    req.decoded= decoded
+    next()
+    // console.log(decoded); // bar
+  });
+}
 
 async function run() {
   try {
@@ -98,13 +112,19 @@ async function run() {
     });
 
 
-    app.get("/booking", async (req, res) => {
+    app.get("/booking", verifyJWT, async (req, res) => {
       const patient = req.query.patient;
-      const authorization = req.headers.authorization
-      console.log('auth header', authorization);
-      const query = { patient: patient };
-      const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings);
+      const decodedEmail = req.decoded.email
+      if(patient === decodedEmail){
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings);
+      }
+      else{
+        return res.status(403).send({message:'forbidden access'})
+      }
+     
+      
     });
 
 
